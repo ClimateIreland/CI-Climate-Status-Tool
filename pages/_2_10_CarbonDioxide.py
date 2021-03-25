cab00ad1b78710a0919c6c63ce75ce673d493bba
jamesfitton <james.m.fitton@gmail.com>
2021-03-25 12:46:16 +0000

    Include trend captions, subscript, updated chart function names

--- pages/_2_10_CarbonDioxide.py
+++ pages/_2_10_CarbonDioxide.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 import pathlib
 import page_builder as pb
 from settings import *
-from charts import empty_chart
+from charts import empty_chart, figure_2_18
 
 chapter_num = '2.10'
 bannerImgSrc = IMAGES_PATH + \
@@ -14,26 +14,26 @@ bannerImgSrc = IMAGES_PATH + \
 bannerImgCredit = 'Pixabay.com'
 
 introText = """
-Carbon dioxide (CO2) is the most important greenhouse gas in the atmosphere apart from water vapour. 
-Some CO2 is removed by plants, soils and the ocean, the rest remains in the atmosphere for centuries 
+Carbon dioxide (CO\u2082) is the most important greenhouse gas in the atmosphere apart from water vapour. 
+Some CO\u2082 is removed by plants, soils and the ocean, the rest remains in the atmosphere for centuries 
 and is one of the main drivers of climate change.  Human activities such as fossil fuel combustion, 
-cement production, deforestation, vegetation fires and land-use changes all contribute to CO2 emissions. 
+cement production, deforestation, vegetation fires and land-use changes all contribute to CO\u2082 emissions. 
         """
 bulletPoint1 = """
-The annual rate of increase in atmospheric CO2 over the past 60 years is about 
+The annual rate of increase in atmospheric CO\u2082 over the past 60 years is about 
 100 times faster than previous natural increases, 
 such as those that occurred at the end of the last ice age 11,000-17,000 years ago.
         """
 bulletPoint2 = """
-Atmospheric CO\u2082 concentrations of 413 ppm at Mace Head in 2018 are estimated to be more than 50% higher than those of the pre-industrial era.
+Atmospheric CO\u2082 concentrations of 413 ppm at Mace Head in 2018 are estimated to be more than 50% higher than those of the pre-industrial era. 
         """
 bulletPoints = [bulletPoint1, bulletPoint2]
 
-trendChartTitle = ''
-trendChart = empty_chart()
+trendChartTitle = 'Carbon Dioxide (CO\u2082) Concentration (1958-2018)'
+trendChart = figure_2_18()
 
 trendCaption = """
-
+        Monthly mean concentration of carbon dioxide at Mauna Loa, Hawaii (1958–2012) and Mace Head Research Station, Ireland (1992–2018). 
         """
 
 infrastructureText = """
--- pages/_2_11_Methane.py
+++ pages/_2_11_Methane.py
@@ -6,29 +6,29 @@ import dash_bootstrap_components as dbc
 import pathlib
 import page_builder as pb
 from settings import *
-from charts import empty_chart, figure_2_11
+from charts import empty_chart, figure_2_20
 
 chapter_num = '2.11'
 bannerImgSrc = IMAGES_PATH+'AtmosphericSections/Methane_Ned_Dwyer_P1000356.JPG'
 bannerImgCredit = 'Ned Dwyer'
 
 introText = """
-Methane (CH4) is the third most important greenhouse gas in the atmosphere after CO2 and water vapour.   
-Although estimated to have a global warming potential evaluated over 100 years of approximately 28 times that of CO2, methane does not persist for much more than a decade in the atmosphere.  
+Methane (CH\u2084) is the third most important greenhouse gas in the atmosphere after CO\u2082 and water vapour.   
+Although estimated to have a global warming potential evaluated over 100 years of approximately 28 times that of CO\u2082, methane does not persist for much more than a decade in the atmosphere.  
 Approximately 60% of methane emissions are due to human activities, the remaining 40% are due to natural processes. 
         """
 bulletPoint1 = """
-In Ireland, agriculture contributes almost 93% of total CH4 emissions, with the remainder due to waste management (5%) and the energy sectors. 
+In Ireland, agriculture contributes almost 93% of total CH\u2084 emissions, with the remainder due to waste management (5%) and the energy sectors. 
         """
 bulletPoint2 = """
-Following a stable period in the late 1990s and early 2000s there has been a 4% increase in CH4 concentrations since 2007 observed at Mace Head. 
+Following a stable period in the late 1990s and early 2000s there has been a 4% increase in CH\u2084 concentrations since 2007 observed at Mace Head. 
         """
 bulletPoints = [bulletPoint1, bulletPoint2]
-trendChartTitle = 'Methane (CH4) Concentration at Mace Head (1987-2018)'
-trendChart = figure_2_11()
+trendChartTitle = 'Methane (CH\u2084) Concentration at Mace Head (1987-2018)'
+trendChart = figure_2_20()
 
 trendCaption = """
-
+        Monthly mean methane concentration observed at Mace Head Research Station (1987–2018).     
         """
 
 infrastructureText = """
