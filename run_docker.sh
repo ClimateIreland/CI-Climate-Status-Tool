d4d7f0c36b112571bf213b251a5a8d4440097e79
dancasey-ie <info@dancasey.ie>
2021-03-31 22:41:55 +0100

    Change docker-compose strategy

--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "killing old docker processes"
-docker-compose rm -fs
-
-echo "building docker containers"
-docker-compose up --build -d
-
-echo "Dash is running on http://0.0.0.0:8080/"
