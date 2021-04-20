b9fdd5e9c816ee4ecb7730b94b023be9e22461c4
dancasey-ie <info@dancasey.ie>
2021-04-20 15:50:40 +0100

    Comment out custom.js

--- dash_app/assets/custom.js
+++ dash_app/assets/custom.js
@@ -1,9 +1,9 @@
-try {
-document.domain = "dash.climateireland.ie";
-console.log('set domain');
-console.log(document.domain);
-}
-catch(err) {
-console.log("Tried to set domain name");
-console.log(err.message);
-}
+// try {
+// document.domain = "dash.climateireland.ie";
+// console.log('set domain');
+// console.log(document.domain);
+// }
+// catch(err) {
+// console.log("Tried to set domain name");
+// console.log(err.message);
+// }
