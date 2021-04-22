abb84c08ee41b61b6fcf67b966e00c604ac5bea8
dancasey-ie <info@dancasey.ie>
2021-04-22 14:14:56 +0100

    Try fix on load

--- dash_app/assets/custom.js
+++ dash_app/assets/custom.js
@@ -7,38 +7,40 @@
 // console.log("Tried to set domain name");
 // console.log(err.message);
 // }
-// var iframe = document.getElementById('dashIFrame');
-// var doc = (iframe.contentDocument)? iframe.contentDocument: iframe.contentWindow.document;
+document.onload(function() {
+    var iframe = document.getElementById('dashIFrame');
+    var doc = (iframe.contentDocument)? iframe.contentDocument: iframe.contentWindow.document;
 
-// var anchors = doc.getElementsByTagName('a');
-// for (var i = 0; i < anchors.length; i++)
-//     anchors[i].target = '_parent';
+    var anchors = doc.getElementsByTagName('a');
+    for (var i = 0; i < anchors.length; i++)
+        anchors[i].target = '_parent';
+    }
+    )
 
-
-$(function() {
-    var iframeOffset = $("#dashIFrame", window.parent.document).offset();
-    $("a").each(function () {
-        var link = $(this);
-        var href = link.attr("href");
-        if (href && href[0] == "#") {
-            var name = href.substring(1);
-            $(this).click(function () {
-                var nameElement = $("[name='" + name + "']");
-                var idElement = $("#" + name);
-                var element = null;
-                if (nameElement.length > 0) {
-                    element = nameElement;
-                } else if (idElement.length > 0) {
-                    element = idElement;
-                }
+// $(function() {
+//     var iframeOffset = $("#dashIFrame", window.parent.document).offset();
+//     $("a").each(function () {
+//         var link = $(this);
+//         var href = link.attr("href");
+//         if (href && href[0] == "#") {
+//             var name = href.substring(1);
+//             $(this).click(function () {
+//                 var nameElement = $("[name='" + name + "']");
+//                 var idElement = $("#" + name);
+//                 var element = null;
+//                 if (nameElement.length > 0) {
+//                     element = nameElement;
+//                 } else if (idElement.length > 0) {
+//                     element = idElement;
+//                 }
   
-                if (element) {
-                    var offset = element.offset();
-                    window.parent.scrollTo(offset.left, offset.top + iframeOffset.top);
-                }
+//                 if (element) {
+//                     var offset = element.offset();
+//                     window.parent.scrollTo(offset.left, offset.top + iframeOffset.top);
+//                 }
   
-                return false;
-            });
-        }
-    });
-  });
+//                 return false;
+//             });
+//         }
+//     });
+//   });
