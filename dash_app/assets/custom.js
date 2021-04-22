179c5e21b1c2366f3f48e73cf1794a1077535602
dancasey-ie <info@dancasey.ie>
2021-04-22 14:27:23 +0100

    Remove scripts

--- dash_app/assets/custom.js
+++ dash_app/assets/custom.js
@@ -7,16 +7,18 @@
 // console.log("Tried to set domain name");
 // console.log(err.message);
 // }
-document.onload=function() {
-    var iframe = document.getElementById('dashIFrame');
-    var doc = (iframe.contentDocument)? iframe.contentDocument: iframe.contentWindow.document;
 
-    var anchors = doc.getElementsByTagName('a');
-    for (var i = 0; i < anchors.length; i++)
-        anchors[i].target = '_parent';
-    };
+// ////////////////////////////////////
+// document.onload=function() {
+//     var iframe = document.getElementById('dashIFrame');
+//     var doc = (iframe.contentDocument)? iframe.contentDocument: iframe.contentWindow.document;
+
+//     var anchors = doc.getElementsByTagName('a');
+//     for (var i = 0; i < anchors.length; i++)
+//         anchors[i].target = '_parent';
+//     };
     
-
+// ////////////////////////////////////
 // $(function() {
 //     var iframeOffset = $("#dashIFrame", window.parent.document).offset();
 //     $("a").each(function () {
