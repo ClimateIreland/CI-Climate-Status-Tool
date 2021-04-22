521065a4fac716bd1a1bb0a53e21e1c427774679
dancasey-ie <info@dancasey.ie>
2021-04-22 14:17:54 +0100

    Try fix on load 2

--- dash_app/assets/custom.js
+++ dash_app/assets/custom.js
@@ -7,15 +7,15 @@
 // console.log("Tried to set domain name");
 // console.log(err.message);
 // }
-document.onload(function() {
+document.onload=function() {
     var iframe = document.getElementById('dashIFrame');
     var doc = (iframe.contentDocument)? iframe.contentDocument: iframe.contentWindow.document;
 
     var anchors = doc.getElementsByTagName('a');
     for (var i = 0; i < anchors.length; i++)
         anchors[i].target = '_parent';
-    }
-    )
+    };
+    
 
 // $(function() {
 //     var iframeOffset = $("#dashIFrame", window.parent.document).offset();
