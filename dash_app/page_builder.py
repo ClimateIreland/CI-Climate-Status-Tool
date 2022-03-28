41af1d35399df1a123fa02b3b4d7e1f485ed32a3
Dan Casey <info@dancasey.ie>
2022-03-28 18:40:48 +0100

    remove feedback call

--- dash_app/page_builder.py
+++ dash_app/page_builder.py
@@ -417,24 +417,5 @@ def build_nav_carousel(chapter_dict):
                                 }
                             }]
                     ))),
-# The row below can be removed after initial release
-            dbc.Row(
-                className='sr-feedback',
-                style={'borderColor': chapter_dict['domain-color']},
-                children=[
-                    dbc.Col([
-                    html.H3('Feedback Appreciated',
-                    style={'color': chapter_dict['domain-color']}),
-                    html.P(
-                    'After taking time to explore the tool, we would appreciate if you could complete a short user survey. The link below is available on all pages.'
-                    ),
-                     html.A(
-                        'Complete our user survey.',
-                        target='_blank',
-                        href='https://surveyhero.com/c/8a7cf6b7',
-                        className='text-center'),
-            ],
-            className='text-center')
-            ])
 
         ])
